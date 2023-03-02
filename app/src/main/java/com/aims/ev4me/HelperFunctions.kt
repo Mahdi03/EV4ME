515f311bfb825ed2aa5127d5b8820697efd043ef
Mahdi Ayman <damahdi03@gmail.com>
2023-03-01 19:00:51 -0800

    TODO: Someone needs to finish the code to send REST API Requests

--- app/src/main/java/com/aims/ev4me/HelperFunctions.kt
+++ app/src/main/java/com/aims/ev4me/HelperFunctions.kt
@@ -1,5 +1,20 @@
 package com.aims.ev4me
 
 import android.util.Patterns
+import com.google.android.gms.maps.model.LatLng
 
 fun CharSequence?.isValidEmail() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+fun convertAddressToLatLng(address: String): LatLng {
+    //TODO: Return LatLng using `sendHTTPRequest`
+    val apiKey = BuildConfig.POSITION_STACK_API_KEY
+    //Use: https://positionstack.com/documentation to set up the right email
+    val latLngString: String = sendHTTPRequest("http://api.positionstack.com/v1/forward/$?access_key=$apiKey&query=$address")
+    return LatLng(0.0, 0.0);
+}
+
+fun sendHTTPRequest(url: String): String {
+    //TODO: Implement this, try not to use any 3rd party libraries
+    // asynchronous if possible
+    return ""
+}
