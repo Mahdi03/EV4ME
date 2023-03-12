065cda8c68aae0b0b370eac84cfca066dac8f7d0
Mahdi Ayman <damahdi03@gmail.com>
2023-03-12 09:03:09 -0700

    Switched over to Nominatim REST API for location accuracy

--- app/src/main/java/com/aims/ev4me/HelperFunctions.kt
+++ app/src/main/java/com/aims/ev4me/HelperFunctions.kt
@@ -5,48 +5,73 @@ import com.google.android.gms.maps.model.LatLng
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 
-
+/**
+ * Used to check whether a string can be validified as an email
+ */
 fun CharSequence?.isValidEmail() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
+/**
+ * Asynchronous function to convert a string address into
+ * a LatLng object using the Nominatim REST API
+ */
 suspend fun convertAddressToLatLng(address: String): LatLng {
-    val apiKey = BuildConfig.POSITION_STACK_API_KEY
-    val response = sendHTTPRequestForward("http://api.positionstack.com/v1/forward?access_key=$apiKey", address)
-
-    //val response = sendHTTPRequestForward(" https://nominatim.openstreetmap.org/search?", address)
+    val searchQuery = address.encodeURLQueryComponent()
+    val url = "https://nominatim.openstreetmap.org/search?&q=$searchQuery&format=json"
+    val response = sendHTTPRequest(url)
+    
     //Object that holds every value that the JSON string holds
     //All are initialized as JSONElements because sometimes the API has null values
     //Use .toType() to get it to the desired Type
-    @kotlinx.serialization.Serializable
-    data class Lat_Lng(val latitude: JsonElement, val longitude: JsonElement, val type: JsonElement, val name: JsonElement,
-                       val number: JsonElement, val postal_code: JsonElement, val street: JsonElement, val confidence: JsonElement,
-                       val region: JsonElement, val region_code: JsonElement, val county: JsonElement, val locality: JsonElement,
-                       val administrative_area: JsonElement, val neighbourhood: JsonElement, val country: JsonElement,
-                       val country_code: JsonElement, val continent: JsonElement, val label: JsonElement
+    @Serializable
+    data class LocationObject(
+        val place_id: JsonElement,
+        val licence: JsonElement,
+        val osm_type: JsonElement,
+        val osm_id: JsonElement,
+        val boundingbox: JsonElement,
+        val lat: JsonElement,
+        val lon: JsonElement,
+        val display_name: JsonElement,
+        val `class`: JsonElement,
+        val type: JsonElement,
+        val importance: JsonElement,
+        /*
+        //TODO: The API says that these values might also come
+           with, find a way to handle these as well that doesn't
+           break the deserializer
+        val icon: JsonElement?,
+        val address: JsonElement?,
+        val extratags: JsonElement?
+         */
     )
 
+    //TODO: Add exception handling here in case we fail to 
+    // deserialize the incoming JSON in the specified format 
     //API returns a list of locations so we need to put them in a list
-    @kotlinx.serialization.Serializable
-    data class Lat_LngResponses(val data: List<Lat_Lng>)
+    val locationObjectsArr = Json.decodeFromString<List<LocationObject>>(response) //list of found locations
 
-    val Lat_LngResponse = Json.decodeFromString<Lat_LngResponses>(response) //list of found locations
+    val locationObj = locationObjectsArr[0]   //first found location (most confident)
+    //We need to replace the quotes because our input comes with extra quotes around the strings
+    val latitude = (locationObj.lat).toString().replace("\"", "").toDouble()
+    val longitude = (locationObj.lon).toString().replace("\"", "").toDouble()
 
-    val Lat_Lng1 = Lat_LngResponse.data[0]   //first found location (most confident)
-    val latitude = (Lat_Lng1.latitude).toString().toDouble()
-    val longitude = (Lat_Lng1.longitude).toString().toDouble()
     return LatLng(latitude,longitude)
 }
 
-suspend fun sendHTTPRequestForward(url: String, address: String): String {
-    val client = HttpClient()
-    val searchQuery = address.encodeURLQueryComponent()
 
-    val urlFull = "$url&query=$searchQuery"
-    val response = client.request<String>(urlFull) {
-        method= HttpMethod.Get
+/**
+ * Sends a basic HTTP response and only returns the raw response
+ * for later handling
+ */
+suspend fun sendHTTPRequest(url: String): String {
+    val client = HttpClient()
+    val response = client.request<String>(url) {
+        method = HttpMethod.Get
     }
     client.close()
     return response
